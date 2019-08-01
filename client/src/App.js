2d136613e2727c650d813cf861a38672d7b0dcca
Sualeh Ali <smsali97@gmail.com>
2019-08-01 23:52:46 +0500

    Added Client-side/React Files

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+import Contacts from './components/Contacts';
+
+
+function App() {
+  return (
+    <div className="containter-fluid">
+        <nav>
+          <div className="nav-wrapper center-align">
+            <a href="/" className="brand-logo">Contacts</a>
+          </div>
+        </nav>
+        <div className="row">
+          <Contacts />
+        </div>
+    </div>
+  );
+}
+
+export default App;
